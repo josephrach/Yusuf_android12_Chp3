d4af49beb137c4c9fbe96d446e1746f5c0373b3a
Sir Joseph <sirjosephholmes@gmail.com>
2022-03-08 00:40:30 +0700

    udah jadi ni

--- Challenge_chp3.kt
+++ Challenge_chp3.kt
@@ -1,13 +1,54 @@
+import java.util.Scanner;
+
 fun main(args: Array<String>) {
-    var input = Saya()
-}
+    val input = Scanner(System.`in`)
 
-class Saya {
-    var input : String = readLine()
+    println("""
+    ===========================
+    GAME SUIT TERMINAL VERSION
+    ===========================
+    1. Masukan pemain 1:
+    """)
+    var pemain1 = input.nextLine()
+    println("2. Masukin pemain 2:")
+    var pemain2 = input.nextLine()
 
-}
-
-class Lawan {
+    // println("$pemain1, $pemain2")
+    if (pemain1 == pemain2){
+        println("""
+        Hasil:
+        DRAW!
+        """)
+    }else if(pemain1 == "gunting" && pemain2 == "kertas"){
+        println("""
+        Hasil:
+        pemain 1 MENANG!
+        """)
+    }else if(pemain1 == "gunting" && pemain2 == "batu"){
+        println("""
+        Hasil:
+        pemain 2 MENANG!
+        """)
+    }else if(pemain1 == "kertas" && pemain2 == "gunting"){
+        println("""
+        Hasil:
+        pemain 2 MENANG!
+        """)
+    }else if(pemain1 == "kertas" && pemain2 == "batu"){
+        println("""
+        Hasil:
+        pemain 1 MENANG!
+        """)
+    }else if(pemain1 == "batu" && pemain2 == "gunting"){
+        println("""
+        Hasil:
+        pemain 1 MENANG!
+        """)
+    }else{
+        println("""
+        Hasil:
+        pemain 2 MENANG!
+        """)
+    }
     
 }
-
